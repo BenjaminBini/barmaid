76f2c91709ade13fe0debf7d44e384e4210cd614
BenjaminBini <benjamin.bini@gmail.com>
2016-06-30 11:00:31 +0200

    Handle options errors in server-manager
    And consider that an exception thrown in "server" is a port issue

--- src/main/servers-manager.js
+++ src/main/servers-manager.js
@@ -11,10 +11,21 @@ class ServersManager {
     if (this.servers.filter(s => s.port === options.port).length > 0) {
       throw new Error('This port is already in use.');
     }
+    // Path and port are mandatory
+    if (!options.path && !options.port) {
+      throw new Error('You have to chose a directory and a port');
+    }
+    if (!options.path) {
+      throw new Error('You have to chose a directory');
+    }
+    if (!options.port) {
+      throw new Error('You have to chose a port'); 
+    }
     try {
       let server = new Server(options);
       this.servers.push(server);
-    } catch(_) {
+    } catch(e) {
+      console.log(e);
       throw new Error('This port is already in use.');
     }
   }
